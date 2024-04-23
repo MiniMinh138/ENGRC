a8c2febca0b042db86ba3dadce0fab2451473541
Minh <man229@cornell.edu>
2024-04-23 15:41:50 -0400

    Created Portfolio layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,13 @@
-function filterSelection(tag) {
-  var items = document.getElementsByClassName('item');
+function filter(tag) {
+  var items = document.querySelectorAll('.item');
 
-  // Show all items if 'all' is selected
-  if (tag === 'all') {
-    for (var i = 0; i < items.length; i++) {
-      items[i].classList.add('show');
+  items.forEach(function (item) {
+    if (tag === 'all' || item.classList.contains(tag)) {
+      item.classList.remove('hide'); // Show item
+    } else {
+      item.classList.add('hide'); // Hide item
     }
-    return;
-  }
-
-  // Hide all items
-  for (var i = 0; i < items.length; i++) {
-    items[i].classList.remove('show');
-  }
-
-  // Show items with the selected tag
-  var selectedItems = document.getElementsByClassName(tag);
-  for (var i = 0; i < selectedItems.length; i++) {
-    selectedItems[i].classList.add('show');
-  }
+  });
 }
+
+
